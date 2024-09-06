999f69407119a5ae88cb14b798f0350f0b852675
olivier_plessis <hello@olivierplessis.fr>
2024-09-06 06:27:28 +0200

    add import in app_theme

--- bricks/scaffold_app/__brick__/{{name.snakeCase()}}/lib/src/core/theme/app_theme_data.dart
+++ bricks/scaffold_app/__brick__/{{name.snakeCase()}}/lib/src/core/theme/app_theme_data.dart
@@ -1,5 +1,7 @@
 import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
+import 'package:{{name.snakeCase()}}/src/core/theme/dark_theme.dart';
+import 'package:{{name.snakeCase()}}/src/core/theme/light_theme.dart';
 
 extension AppThemeData on ThemeData {
   static ThemeData light() =>
